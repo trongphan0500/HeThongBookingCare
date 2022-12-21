69a3c1e07d1b027bf64d6c6cfc372cc8052e87e5
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-12-21 10:37:26 +0700

    Initial commit

---
+++ src/test/java/com/example/hethongbookingcare/HeThongBookingCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hethongbookingcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeThongBookingCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
